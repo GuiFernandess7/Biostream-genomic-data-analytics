c010cfa31f6bdf8b22bb7e7e2e4d20248d77bc93
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-06-25 14:16:07 -0300

    feat: Message queue consumer added

---
+++ queue/src/main.py
@@ -0,0 +1,16 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='data-stream')
+
+def callback(ch, method, properties, body):
+    message = body.decode('utf-8')
+    print(f"Received: {message} \n")
+    print("\n")
+
+channel.basic_consume(queue='data-stream', on_message_callback=callback, auto_ack=True)
+
+print('Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
