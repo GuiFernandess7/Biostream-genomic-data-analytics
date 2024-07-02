21194f6ca8a0ff41f0c98c73a92604c4c623a6c6
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-02 18:55:10 -0300

    feat: UI updated

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -6,6 +6,8 @@ import plotly.graph_objects as go
 from collections import Counter
 from Bio import Align
 from Bio.Seq import Seq
+import traceback
+import re
 
 st.set_page_config(page_title="Biostream Dash", page_icon=":bar_chart:", layout="wide")
 st.markdown(
@@ -20,7 +22,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-st.write('Waiting for messages...')
+
 if "sequences" not in st.session_state:
         st.session_state.sequences = []
 
@@ -31,21 +33,55 @@ colors = {
     'G': 'orange'
 }
 
-async def consume_messages():
-    connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
-    channel = await connection.channel()
+def validate_websocket_url(url: str):
+    websocket_pattern = r"^(ws|wss):\/\/([a-zA-Z0-9\-\.]+)(:\d+)?(\/[^\s]*)?$"
+    return re.match(websocket_pattern, url) is not None
 
-    queue = await channel.declare_queue("data-stream")
+def validate_amqp_url(url: str):
+    amqp_pattern = r"^amqp:\/\/(?:[^:@\/]+:[^:@\/]+@)?[^:\/]+\.[^:\/]+(:\d+)?(\/[^\s]*)?$"
+    return re.match(amqp_pattern, url) is not None
 
-    async with queue.iterator() as queue_iter:
-        async for message in queue_iter:
-            async with message.process():
-                if message.body.decode('utf-8').startswith(">"):
-                    st.session_state.sequences.append(message.body.decode('utf-8'))
-                else:
-                    st.session_state.sequences.extend(message.body.decode('utf-8'))
+def validate_queue_name(name: str):
+    name_pattern = r'^[A-Za-z0-9_-]{1,255}$'
 
-async def show_nucleotide_distribution(graph): #_and_dimer_distribution(graph):
+    if re.match(name_pattern, name):
+        return True
+    else:
+        return False
+
+async def consume_messages(address: str, queue_name: str):
+    connection = await aio_pika.connect_robust(address)
+    try:
+        print("Conexão estabelecida com sucesso!")
+
+        channel = await connection.channel()
+
+        queue_name = "data-stream"
+        queue = await channel.declare_queue(queue_name)
+
+        async with queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                async with message.process():
+                    body = message.body.decode('utf-8')
+
+                    if body.startswith(">"):
+                        st.session_state.sequences.append(body)
+                    else:
+                        st.session_state.sequences.extend(body)
+
+    except aio_pika.exceptions.AMQPError as e:
+        st.error(f"Error: {e}")
+        traceback.print_exc()
+
+    except Exception as e:
+        st.error(f"Unexpected Error: {e}")
+        traceback.print_exc()
+
+    finally:
+        if connection:
+            await connection.close()
+
+async def show_nucleotide_distribution(graph):
     while True:
         if "sequences" in st.session_state and st.session_state.sequences:
             all_sequences = ''.join(seq for seq in st.session_state.sequences if not seq.startswith(">"))
@@ -61,9 +97,8 @@ async def show_nucleotide_distribution(graph): #_and_dimer_distribution(graph):
                 template='plotly_white'
             )
 
-            col1, _ = st.columns(2)
-            with col1:
-                graph.plotly_chart(fig_nucleotides, use_container_width=True)
+
+            graph.plotly_chart(fig_nucleotides, use_container_width=True)
 
         await asyncio.sleep(0.5)
 
@@ -72,7 +107,6 @@ async def dimer_distribution(graph):
         if "sequences" in st.session_state and st.session_state.sequences:
             all_sequences = ''.join(seq for seq in st.session_state.sequences if not seq.startswith(">"))
 
-            # Dimer distribution
             dimers = [all_sequences[i:i+2] for i in range(len(all_sequences)-1)]
             dimer_counts = Counter(dimers)
             fig_dimers = go.Figure()
@@ -91,98 +125,51 @@ async def dimer_distribution(graph):
 
         await asyncio.sleep(0.5)
 
-async def main():
+async def main(address: str = None, queue_name: str = None):
     col1, col2 = st.columns(2)
     with col1:
         graph = st.empty()
     with col2:
         graph2 = st.empty()
 
-    task1 = asyncio.create_task(consume_messages())
+    task1 = asyncio.create_task(consume_messages(address, queue_name))
     task2 = asyncio.create_task(show_nucleotide_distribution(graph))
     task3 = asyncio.create_task(dimer_distribution(graph2))
     await asyncio.gather(task1, task2, task3)
 
 if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("Closing Loop")
-        loop.close()
+    col1, col2 = st.columns([1, 1])
+    tab1, tab2 = col1.tabs(["File upload", "Websocket | RabbitMQ"])
 
+    with tab2:
+        status = st.empty()
+        address = st.text_input("Address")
+        st.caption('Examples: [amqp://user:pass@broker.example.com:5672/vhost, amqp://127.0.0.1:5672]')
+        queue_name = st.text_input("Queue name")
+        start_conn_btt = st.button(label="Run", key="run-addr-button")
 
-""" async def show_trimer_distribution():
-    all_sequences = ''.join(seq for seq in st.session_state.sequences if not seq.startswith(">"))
-    trimers = [all_sequences[i:i+3] for i in range(len(all_sequences)-2)]
-    trimer_counts = Counter(trimers)
-    fig_trimers = go.Figure()
-    for trimer, count in trimer_counts.items():
-        fig_trimers.add_trace(go.Bar(x=[trimer], y=[count]))
-    fig_trimers.update_layout(
-        title='Trimer Distribution',
-        xaxis_title='Trimer',
-        yaxis_title='Count',
-        template='plotly_white'
-    )
-    with empty.container():
-        st.plotly_chart(fig_trimers, use_container_width=True)
-    await asyncio.sleep(0.5)
+    with tab1:
+        status = st.empty()
+        st.session_state.file = st.file_uploader(label="Upload Genomic Sequence file (FASTA files only).")
+        file_check_btt = st.button(label="Run", key="run-file-button")
 
-async def show_codon_distribution():
-    all_sequences = ''.join(seq for seq in st.session_state.sequences if not seq.startswith(">")).replace('T', 'U')
-    codons = [all_sequences[i:i+3] for i in range(0, len(all_sequences) - 2, 3)]
-    codon_counts = Counter(codons)
-    fig_codons = go.Figure()
-    for codon, count in codon_counts.items():
-        fig_codons.add_trace(go.Bar(x=[codon], y=[count]))
-    fig_codons.update_layout(
-        title='Codon Distribution',
-        xaxis_title='Codon',
-        yaxis_title='Count',
-        template='plotly_white'
-    )
-    with empty.container():
-        st.plotly_chart(fig_codons, use_container_width=True)
-    await asyncio.sleep(0.5)
+    if start_conn_btt:
+        if not validate_amqp_url(address) and not validate_websocket_url(address):
+            st.error("Invalid address")
 
-def get_kmers(sequence, k):
-    return [sequence[i:i+k] for i in range(len(sequence) - k + 1)]
+        if not validate_queue_name(queue_name):
+            st.error("Invalid queue name")
 
-async def show_kmer_distribution(k=6):
-    all_sequences = ''.join(seq for seq in st.session_state.sequences if not seq.startswith(">"))
-    kmers = get_kmers(all_sequences, k)
-    kmer_counts = Counter(kmers)
-    fig_kmers = go.Figure()
-    for kmer, count in kmer_counts.items():
-        fig_kmers.add_trace(go.Bar(x=[kmer], y=[count]))
-    fig_kmers.update_layout(
-        title=f'{k}-mer Distribution',
-        xaxis_title=f'{k}-mer',
-        yaxis_title='Count',
-        template='plotly_white'
-    )
-    with empty.container():
-        st.plotly_chart(fig_kmers, use_container_width=True)
-    await asyncio.sleep(0.5)
-
-async def show_variability_analysis(reference_sequence):
-    all_sequences = [Seq(seq) for seq in st.session_state.sequences if not seq.startswith(">")]
-    aligner = Align.PairwiseAligner()
-    aligner.mode = 'global'
-    fig_variability = go.Figure()
-    for sequence in all_sequences:
-        score = aligner.score(reference_sequence, sequence)
-        fig_variability.add_trace(go.Scatter(x=[sequence], y=[score], mode='markers'))
-    fig_variability.update_layout(
-        title='Genetic Variability',
-        xaxis_title='Sequence',
-        yaxis_title='Alignment Score',
-        template='plotly_white'
-    )
-    with empty.container():
-        st.plotly_chart(fig_variability, use_container_width=True)
-    await asyncio.sleep(0.5) """
+        else:
+            stop_button = col2.button("Stop", key="stop-button")
+            col2.success("Receiving data...")
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            try:
+                asyncio.run(main(address))
+                if stop_button:
+                    loop.close()
+            except KeyboardInterrupt:
+                pass
+            finally:
+                loop.close()
